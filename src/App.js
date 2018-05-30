6c14516e3e58c0b630c4a1b6259bf5dfad3189db
devingprice <devingprice@gmail.com>
2018-05-30 15:47:32 -0400

    Make banner responsive

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import './App.css';
 import Tile from './tile';
 
 class App extends Component {
+  /** TODO: How yt does grid rows/ hidden items
+   * row contents > row renderer (set width) > 
+   * left right arrow divs (moves translateX of items)
+   *  + scroll container (set width) (hide overflow) > 
+   * items (huge width) (nowrap + will-change) [transform:translateX(0)]
+  **/
+
   render() {
     return (
       <div className="App">
@@ -18,8 +25,12 @@ class App extends Component {
             <div className="content">
 
               <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1 className="App-title">Welcome to React</h1>
+                <div className="App-banner">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <h1 className="App-title">Welcome to React</h1>
+                </div>
+
+                <div className="App-channel-info"></div>
               </header>
 
               <div className="App-body">
@@ -28,6 +39,7 @@ class App extends Component {
                     <h2 className="shelfTitle">
                     The Philip DeFranco Show (Every Mon-Tues-Wed-Thursday!)
                     </h2>
+                    
                     <div className="grid">
                       <Tile />
                       <Tile />
