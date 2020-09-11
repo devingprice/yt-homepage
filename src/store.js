0be5c7d974c9edfaeeda2406aa851bfd1e2e1efd
Devin Price <devingprice@gmail.com>
2020-09-11 00:31:19 -0400

    Remove redux-observable rxjs rxjs-compat

--- src/store.js
+++ src/store.js
@@ -2,14 +2,11 @@ import { createBrowserHistory } from 'history'
 import { applyMiddleware, compose, createStore } from 'redux'
 import { routerMiddleware } from 'connected-react-router';
 
-import { createEpicMiddleware } from 'redux-observable';
 import thunkMiddleware from 'redux-thunk';
 
-import rootEpic from './epics';
 import rootReducer from './reducers';
 
 export const history = createBrowserHistory();
-const epicMiddleware = createEpicMiddleware();
 
 const store = createStore(
     rootReducer(history), // root reducer with router state
@@ -17,12 +14,10 @@ const store = createStore(
     compose(
         applyMiddleware(
             thunkMiddleware,
-            epicMiddleware,
             routerMiddleware(history), // for dispatching history actions
             // ... other middlewares ...
 
         ),
     ),
 );
-epicMiddleware.run(rootEpic);
 export default store;
